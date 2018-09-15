bc123ad072b43da2d2be5d1263d5f7bd19d7e2d0
adityanaik <adityan9900@gmail.com>
2018-09-15 19:32:25 -0400

    Updated CompanySummary screen

--- app/src/main/java/com/helloworld/candor/candor/CompanySummaryActivity.java
+++ app/src/main/java/com/helloworld/candor/candor/CompanySummaryActivity.java
@@ -1,21 +1,35 @@
 package com.helloworld.candor.candor;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Window;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class CompanySummaryActivity extends AppCompatActivity {
 
-    RelativeLayout gradeLayout = findViewById(R.id.gradeLayout);
+    RelativeLayout gradeLayout;
+    String companyName, letterGrade;
+    TextView companyNameView, letterGradeView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_company_summary);
         System.out.println(getActionBar());
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        gradeLayout = findViewById(R.id.gradeLayout);
+        gradeLayout.setBackgroundColor(Color.RED);
+        companyNameView = findViewById(R.id.companyNameView);
+        letterGradeView = findViewById(R.id.letterGradeView);
+        companyName = "C O M P A N Y";
+        letterGrade = "A+";
+        companyNameView.setText(companyName);
+        letterGradeView.setText(letterGrade);
     }
 
 }
