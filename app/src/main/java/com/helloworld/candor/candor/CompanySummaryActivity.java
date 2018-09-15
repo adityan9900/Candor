083b6b1aef5527235f654664e75391c06f8ae12b
adityanaik <adityan9900@gmail.com>
2018-09-15 18:43:55 -0400

    Fixed action bar for CompanySummaryActivity

--- app/src/main/java/com/helloworld/candor/candor/CompanySummaryActivity.java
+++ app/src/main/java/com/helloworld/candor/candor/CompanySummaryActivity.java
@@ -2,14 +2,17 @@ package com.helloworld.candor.candor;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
 
-public class CompanySummaryActivity extends Activity {
+public class CompanySummaryActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_company_summary);
-        getActionBar().setDisplayHomeAsUpEnabled(true);
+        System.out.println(getActionBar());
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
 }
