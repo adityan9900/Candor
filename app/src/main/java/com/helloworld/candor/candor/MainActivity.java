d7ea3270da54812151d76d4e6e6f754033e84250
adityanaik <adityan9900@gmail.com>
2018-09-15 17:35:20 -0400

    Added button listener to main activity

--- app/src/main/java/com/helloworld/candor/candor/MainActivity.java
+++ app/src/main/java/com/helloworld/candor/candor/MainActivity.java
@@ -1,16 +1,30 @@
 package com.helloworld.candor.candor;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.beardedhen.androidbootstrap.TypefaceProvider;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button submitCompanyButton = findViewById(R.id.submitCompanyButton);
+    final int COMPANY_SUMMARY_REQUEST_CODE = 1;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         TypefaceProvider.registerDefaultIconSets();
+
+        submitCompanyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(v.getContext(), CompanySummaryActivity.class);
+                startActivityForResult(myIntent, COMPANY_SUMMARY_REQUEST_CODE);
+            }
+        });
     }
 }
