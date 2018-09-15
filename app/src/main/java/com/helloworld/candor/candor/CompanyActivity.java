38cc832e3cc8af557fa935cad29ada02196349c0
dadeej <joseph71@purdue.edu>
2018-09-15 16:47:30 -0400

    added a couple methods

--- app/src/main/java/com/helloworld/candor/candor/CompanyActivity.java
+++ app/src/main/java/com/helloworld/candor/candor/CompanyActivity.java
@@ -1,5 +1,35 @@
 package com.helloworld.candor.candor;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Iterator;
+import java.util.Set;
+
 public class CompanyActivity {
-    
+    public static final double aPlus = 60.00;
+    public static final double am = 48.30;
+    public static final double aMinus = 43.20;
+    public static final double bPlus = 38.10;
+    public static final double bm = 31.80;
+    public static final double bMinus = 27.50;
+    public static final double cPlus = 20.70;
+    public static final double cm = 15.00;
+    public static final double cMinus = 9.80;
+    public static final double dPlus = 5.60;
+    public static final double dm = 3.90;
+    public static final double dMinus = 0.80;
+    public static final double fm = 0.00;
+
+    public CompanyActivity () {
+        HashMap<String, Double> hmap = new HashMap<>();
+        hmap.put("Cisco Systems", 83.70);
+        hmap.put("Ecolab", 81.60);
+        hmap.put("Hasbro", 80.00);
+    }
+
+    public String getScore() {
+
+
+        return "";
+    }
 }
