be0d264a59bd214ddb2fd90bcdcd0c8d06a89b84
Ashwyn8903 <khandel5@purdue..edu>
2018-09-15 15:36:40 -0400

    Adding bootstrap

--- app/src/main/java/com/helloworld/candor/candor/MainActivity.java
+++ app/src/main/java/com/helloworld/candor/candor/MainActivity.java
@@ -3,11 +3,13 @@ package com.helloworld.candor.candor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.beardedhen.androidbootstrap.TypefaceProvider;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        TypefaceProvider.registerDefaultIconSets();
     }
 }
