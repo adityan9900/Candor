005ad6c8dcf7a62599677d833aded4b3ec3e9c6c
Ashwyn8903 <khandel5@purdue..edu>
2018-09-16 11:23:03 -0400

    Updates for activity_reward

---
+++ app/src/main/java/com/helloworld/candor/candor/community_activity.java
@@ -0,0 +1,13 @@
+package com.helloworld.candor.candor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class community_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_community_activity);
+    }
+}
