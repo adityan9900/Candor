4a672c681771026cbc7440f34f82b2b54cc34b3a
dadeej <joseph71@purdue.edu>
2018-09-16 08:05:49 -0400

    some progress

--- app/src/main/java/com/helloworld/candor/candor/MoreInformationActivity.java
+++ app/src/main/java/com/helloworld/candor/candor/MoreInformationActivity.java
@@ -2,15 +2,32 @@ package com.helloworld.candor.candor;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.widget.TextView;
+import android.widget.EditText;
 import android.support.v7.app.AppCompatActivity;
 
 public class MoreInformationActivity extends AppCompatActivity {
+
+    TextView rankText;
+    TextView percentText;
+    TextView compName;
+    EditText descripText;
+    CompanyActivity companyActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_more_information);
         //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        companyActivity = new CompanyActivity();
+
+        rankText = findViewById(R.id.rankText);
+        compName = findViewById(R.id.compName);
+        percentText = findViewById(R.id.percentText);
+        descripText = findViewById(R.id.descripText);
+        
     }
+
 
 }
