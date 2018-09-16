f174ebd55fd45a4f2e7263b16ddf99407e48887c
Ashwyn8903 <khandel5@purdue..edu>
2018-09-16 00:06:13 -0400

    Updates for activity_reward

--- app/src/main/java/com/helloworld/candor/candor/RewardsActivity.java
+++ app/src/main/java/com/helloworld/candor/candor/RewardsActivity.java
@@ -12,6 +12,26 @@ public class RewardsActivity extends AppCompatActivity {
 
 
     ImageButton promoOne;
+    ImageButton revertOne;
+    ImageButton promoTwo;
+    ImageButton revertTwo;
+    ImageButton promoThree;
+    ImageButton revertThree;
+    ImageButton promoFour;
+    ImageButton revertFour;
+    ImageButton promoFive;
+    ImageButton revertFive;
+    ImageButton promoSix;
+    ImageButton revertSix;
+    ImageButton promoSeven;
+    ImageButton revertSeven;
+    ImageButton PromoEight;
+    ImageButton revertEight;
+
+
+    
+
+
 
 
 
@@ -25,6 +45,8 @@ public class RewardsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_rewards);
 
         promoOne = (findViewById(R.id.buttonOne));
+        revertOne = (findViewById(R.id.promoCode));
+
 
         promoOne.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -41,7 +63,16 @@ public class RewardsActivity extends AppCompatActivity {
 
         });
 
+        revertOne.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(promoOne.getVisibility()==View.INVISIBLE) {
+                    findViewById(R.id.buttonOne).setVisibility(View.VISIBLE);
+                }else{
+                    findViewById(R.id.buttonOne).setVisibility(View.INVISIBLE);
+            }
+        }
 
-
-    }
+    });
+}
 }
